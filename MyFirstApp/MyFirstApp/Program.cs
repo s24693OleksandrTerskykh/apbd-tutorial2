629b7c2fd2e27017ef9f12e2b450ad2850c29071
s24693 <s24693@pjwstk.edu.pl>
2024-03-14 23:16:05 +0100

    Implemented method

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modification 1");
 Console.WriteLine("Modification 2");
 Console.WriteLine("Modification 3");
+
+static double GetAverage(int[] numbers)
+{
+    double sum = 0.0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        sum += numbers[i];
+    }    
+    
+    return sum / numbers.Length;
+}
